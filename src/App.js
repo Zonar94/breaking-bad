de418d5792c2cfdd9144c427257ef367f2b6d5b4
zonar <424661@student.fontys.nl>
2021-01-17 21:42:13 +0100

    Added search feature
    Characters can now be filtered by typing into the search bar.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React, {useState, useEffect } from 'react'
 import Header from './components/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
-import './App.css';
-import axios from 'axios';
+import Search from './components/ui/Search'
+import './App.css'
+import axios from 'axios'
 
 const App = () => {
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] =  useState(true)
+  const [items, setItems] = useState([])                          //fill array with the data from the api
+  const [isLoading, setIsLoading] = useState(true)              //checks if the state is loading
+  const [query, setQuery] = useState('')                        //brings up the query from the search
 
-  useEffect(() => {
-   const fetchItems = async () => {
-     const result = await axios('https://www.breakingbadapi.com/api/characters')
+  useEffect(() => {                                            //fetches the items from the api
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`               //breaking bad api
+      )
 
-     console.log(result.data)
-     setItems(result.data)
-     setIsLoading(false)
+      setItems(result.data)
+      setIsLoading(false)
+    }
 
-   }
+    fetchItems()
+  }, [query])
 
-   fetchItems()
-  }, [])
-  return <div className='container'>
-    <Header/>
-    <CharacterGrid isLoading={isLoading} items={items} />
-  </div>
+  return (
+    <div className='container'>
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CharacterItem from './CharacterItem'
 
+
 const CharacterGrid = ({ items, isLoading }) => {
     return isLoading ? (
     <h1>Loading...</h1> 

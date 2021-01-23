3d770cdcd08245563cc0858617806b5c31c46571
zonar <424661@student.fontys.nl>
2021-01-23 02:30:17 +0100

    Added comments in the code

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -11,15 +11,19 @@ const CharacterItem = ({ item }) => {   //structure for the cards
           <h1>{item.name}</h1>
           <ul>
             <li>
+               {/*Loads the actors name*/}
               <strong>Actor Name:</strong> {item.portrayed}
             </li>
             <li>
+               {/*Loads their nickname*/}
               <strong>Nickname:</strong> {item.nickname}
             </li>
             <li>
+               {/*Loads their Birthday*/}
               <strong>Birthday:</strong> {item.birthday}
             </li>
             <li>
+               {/*Loads their status*/}
               <strong>Status:</strong> {item.status}
             </li>
           </ul>
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,9 +1,9 @@
 
 import React, { useState } from 'react'
-
+ {/*This is the query for the searchbar*/}
 const Search = ({ getQuery }) => {            
  const [text, setText] = useState('')
-
+ {/*When it changes it searches in the api*/}
    const onChange = (q) => {
        setText(q)
        getQuery(q)
@@ -15,6 +15,7 @@ const Search = ({ getQuery }) => {
                 <input 
                 type='text'
                 className='form-control'
+                 {/*This shows the search bar text when its empty*/}
                 placeholder='Search Characters'
                 value={text}
                 onChange={(e) => onChange(e.target.value)}
